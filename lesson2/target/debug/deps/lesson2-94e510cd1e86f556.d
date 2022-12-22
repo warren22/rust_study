325b8c3066271837d53591eab40fed73cc1cc3d2
warren <winclass88@126.com>
2022-12-22 21:57:12 +0800

    Struct and Enum study

---
+++ lesson2/target/debug/deps/lesson2-94e510cd1e86f556.d
@@ -0,0 +1,5 @@
+/Users/warren/Documents/rust_study/lesson2/target/debug/deps/lesson2-94e510cd1e86f556: src/main.rs
+
+/Users/warren/Documents/rust_study/lesson2/target/debug/deps/lesson2-94e510cd1e86f556.d: src/main.rs
+
+src/main.rs:
---
+++ lesson2/target/debug/lesson2.d
@@ -0,0 +1,1 @@
+/Users/warren/Documents/rust_study/lesson2/target/debug/lesson2: /Users/warren/Documents/rust_study/lesson2/src/main.rs
